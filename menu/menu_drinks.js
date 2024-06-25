8162c301260ef3183743931e02a395782cd0c11c
ryadra2805 <157661333+ryadra2805@users.noreply.github.com>
2024-06-26 00:37:00 +0800

    Delete menu directory

--- menu/menu_drinks.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const drinksTitle1 = document.querySelector('.drinks_title1');
-
-hamburger.addEventListener('click', () => {
-  drinksTitle1.classList.toggle('collapsed');
-});
--- menu/menu_pastries.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const drinksTitle1 = document.querySelector('.pastries_title1');
-
-hamburger.addEventListener('click', () => {
-  drinksTitle1.classList.toggle('collapsed');
-});
